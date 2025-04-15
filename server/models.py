9525c082afa973cae06c6c0344254ae467fbb375
vintello <vintello@ukr.net>
2025-04-15 15:28:25 +0300

    fix bug

--- server/models.py
+++ server/models.py
@@ -31,7 +31,7 @@ class IPListAccess(SQLModel, table=True):
                                    )
 
 class UserConfigs(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     user_num: int
     file_name: str
 
